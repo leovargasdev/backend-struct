6c5fc744aeaa703c78c802505c3396ccad048095
Leo Vargas <leu1607@hotmail.com>
2020-05-12 15:29:13 -0300

    Criando middleware para centralizar os erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'reflect-metadata';
+import 'express-async-errors';
 
+import AppError from './errors/AppError';
 import routes from './routes';
 import './database';
 
@@ -7,5 +10,21 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+// Centralizando os erros da aplicação
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -1,9 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../models/User';
 
+import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -22,13 +23,13 @@ class CreateSessionService {
     });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
